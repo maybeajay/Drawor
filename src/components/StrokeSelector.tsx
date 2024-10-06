a3239d2e101e8d9c430791f29b3f96fe9dddda15
maybeajay <heyajayramola@gmail.com>
2024-10-06 12:09:17 +0530

    added stroke width selector

---
+++ src/components/StrokeSelector.tsx
@@ -0,0 +1,19 @@
+import React, { Dispatch, SetStateAction } from 'react'
+
+type Props = {
+    currentStroke: number,
+    setcurrentStroke: Dispatch<SetStateAction<number>>
+}
+function StrokeSelector({setcurrentStroke, currentStroke}: Props) {
+    const strokeHandler = (e:React.ChangeEvent<HTMLInputElement>) =>{
+        setcurrentStroke(+e.target.value);
+        console.log(e.target.value)
+    }
+  return (
+    <input type='range' min={5} max={10} onChange={(e)=>strokeHandler(e)}  
+    className='w-20 h-10 absolute top-[4rem] left-[4rem]  myin'
+    />
+  )
+}
+
+export default StrokeSelector
